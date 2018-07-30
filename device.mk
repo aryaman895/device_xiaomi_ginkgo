4cd6c33212e06f87d545172299d29b39e096ccd6
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-30 20:06:19 -0300

    ginkgo: Implement keyhandler for fingerprint shutter
     * prevents fp from triggering other events such as
       vibrate on long press
    
    Fixes: d0e76759b64987e3af18a3290a8b6125ac53f789 ("keylayout: Redirect keycode 96 to CAMERA")
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
