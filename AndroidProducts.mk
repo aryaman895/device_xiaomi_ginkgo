2472f7e079feb51f984eeec7560b2a4e6a2edbe6
Aryaman <aryamansrivastava895@gmail.com>
2020-09-25 16:05:32 +0000

    ginkgo: let's get reloaded

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+ 
+#
+# Copyright (C) 2020 ReloadedOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/reloaded_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    reloaded_ginkgo-user \
+    reloaded_ginkgo-userdebug \
+    reloaded_ginkgo-eng
