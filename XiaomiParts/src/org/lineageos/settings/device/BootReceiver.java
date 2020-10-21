0b02116f6235ed9e12b163d135efacbadc4093e8
OdSazib <odsazib@gmail.com>
2020-10-21 20:30:45 +0000

    ginkgo: XiaomiParts: Add FPS Info Overlay
    * Ported from: https://github.com/Corvus-Devices/device_xiaomi_lavender.git
    * Adapt for ginkgo
    * Fixed position in landscape (ginkgo)
    * Included sepolicy for ginkgo

--- XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
+++ XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
@@ -20,6 +20,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.device.kcal.Utils;
 
@@ -28,6 +30,8 @@ import java.lang.Math.*;
 public class BootReceiver extends BroadcastReceiver implements Utils {
 
     public void onReceive(Context context, Intent intent) {
+
+    	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
@@ -60,5 +64,10 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         FileUtils.setValue(DeviceSettings.USB_FASTCHARGE_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 DeviceSettings.PREF_USB_FASTCHARGE, 0));
         context.startService(new Intent(context, DiracService.class));
+
+        boolean enabled = sharedPrefs.getBoolean(DeviceSettings.PREF_KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+        }
     }
 }
