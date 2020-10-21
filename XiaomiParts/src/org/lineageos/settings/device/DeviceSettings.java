503c77d1c1103fe3c4a94a4562960ba464471406
OdSazib <odsazib@gmail.com>
2020-10-21 20:30:45 +0000

    ginkgo: XiaomiParts: Add FPS Info Overlay
    * Ported from: https://github.com/Corvus-Devices/device_xiaomi_lavender.git
    * Adapt for ginkgo
    * Fixed position in landscape (ginkgo)
    * Also address denials

--- XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
+++ XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
@@ -22,6 +22,10 @@ import android.os.Bundle;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 import org.lineageos.settings.device.kcal.KCalSettingsActivity;
 import org.lineageos.settings.device.preferences.CustomSeekBarPreference;
@@ -55,6 +59,8 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_VIBRATION_STRENGTH = "vibration_strength";
     public static final String VIBRATION_STRENGTH_PATH = "/sys/class/leds/vibrator/vtg_level";
 
+    public static final String PREF_KEY_FPS_INFO = "fps_info";
+
     // value of vtg_min and vtg_max
     public static final int MIN_VIBRATION = 1504;
     public static final int MAX_VIBRATION = 3544;
@@ -62,9 +68,14 @@ public class DeviceSettings extends PreferenceFragment implements
     private Preference mClearSpeakerPref;
     private SecureSettingSwitchPreference mFastcharge;
 
+    private static Context mContext;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences_xiaomi_parts, rootKey);
+
+        mContext = this.getContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         boolean enhancerEnabled;
         try {
@@ -94,6 +105,10 @@ public class DeviceSettings extends PreferenceFragment implements
         if (isAppNotInstalled(DEVICE_DOZE_PACKAGE_NAME)) {
             displayCategory.removePreference(findPreference(PREF_DEVICE_DOZE));
         }
+
+        SwitchPreference fpsInfo = (SwitchPreference) findPreference(PREF_KEY_FPS_INFO);
+        fpsInfo.setChecked(prefs.getBoolean(PREF_KEY_FPS_INFO, false));
+        fpsInfo.setOnPreferenceChangeListener(this);
 
         Preference kcal = findPreference(PREF_DEVICE_KCAL);
 
@@ -158,6 +173,16 @@ public class DeviceSettings extends PreferenceFragment implements
             case PREF_VIBRATION_STRENGTH:
                 double vibrationValue = (int) value / 100.0 * (MAX_VIBRATION - MIN_VIBRATION) + MIN_VIBRATION;
                 FileUtils.setValue(VIBRATION_STRENGTH_PATH, vibrationValue);
+                break;
+
+            case PREF_KEY_FPS_INFO:
+                boolean enabled = (Boolean) value;
+                Intent fpsinfo = new Intent(this.getContext(), FPSInfoService.class);
+                if (enabled) {
+                    this.getContext().startService(fpsinfo);
+                } else {
+                    this.getContext().stopService(fpsinfo);
+                }
                 break;
 
             default:
