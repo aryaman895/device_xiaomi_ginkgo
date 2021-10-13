b83f46375d2f9ca1b53356f57b75a86baded9c88
ujjwalkirorimal <ujjwalkirorimal@gmail.com>
2021-10-13 20:50:01 +0530

    ginkgo: Update DRM HIDL to v1.4
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: wisnuardhi28 <wisnuardi128@gmail.com>
    Signed-off-by: fajar <santuyz321@gmail.com>

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
