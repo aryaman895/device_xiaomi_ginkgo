8616aa9826a3a1af4557126cf28950660d9108da
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2022-01-25 19:06:52 +0000

    Revert "ginkgo: power: Properly handle sustained performance mode"
    This reverts commit c4b05879f4db8fa460e08289e705f0e4c81214c7.

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -91,12 +91,9 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
 #endif
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
-                endAllHints(mHintManager);
                 mHintManager->DoHint("SUSTAINED_PERFORMANCE");
-            } else {
-                mHintManager->EndHint("SUSTAINED_PERFORMANCE");
             }
-            mSustainedPerfModeOn = enabled;
+            mSustainedPerfModeOn = true;
             break;
         case Mode::LOW_POWER:
             if (enabled) {
@@ -108,6 +105,9 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             mBatterySaverOn = enabled;
             break;
         case Mode::LAUNCH:
+            if (mSustainedPerfModeOn) {
+                break;
+            }
             [[fallthrough]];
 #ifndef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
@@ -126,9 +126,7 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::AUDIO_STREAMING_LOW_LATENCY:
             [[fallthrough]];
         default:
-            if (mBatterySaverOn || mSustainedPerfModeOn) {
-                break;
-            }
+            if (mBatterySaverOn) break;
             if (enabled) {
                 mHintManager->DoHint(toString(type));
             } else {
