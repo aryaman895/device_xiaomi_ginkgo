aospa-ginkgo/android_device_xiaomi_ginkgo

ginkgo: Merge system/vendor props into device makefile

device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
-# Copyright (C) 2020-2021 Paranoid Android
+# Copyright (C) 2020-2022 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -25,6 +25,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     vendor.audio.feature.compr_voip.enable=true \
     vendor.audio.feature.spkr_prot.enable=false
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.config.vc_call_vol_default=5 \
+    ro.config.vc_call_vol_steps=6
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
@@ -37,6 +41,21 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.scram.enabled=true \
+    persist.vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false \
+    persist.vendor.bluetooth.modem_nv_support=true \
+    ro.vendor.bluetooth.wipower=false \
+    vendor.qcom.bluetooth.soc=cherokee
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.bluetooth.bqr.event_mask=14 \
+    persist.bluetooth.bqr.min_interval_ms=500
 
 # Camera
 $(call inherit-product, vendor/miuicamera/config.mk)
@@ -54,6 +73,47 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.camera.preview.ubwc=0 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.isp.turbo=1 \
+    persist.vendor.camera.imglib.usefdlite=1 \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.mpo.disabled=1 \
+    persist.vendor.camera.manufacturer=Xiaomi \
+    persist.vendor.camera.stats.test=0 \
+    persist.vendor.camera.awb.sync=2 \
+    persist.vendor.camera.af.sync=2 \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.is_type=4 \
+    persist.vendor.camera.is_type_preview=4 \
+    persist.vendor.camera.gyro.disable=0 \
+    persist.vendor.camera.llnoise=1 \
+    persist.vendor.camera.tnr.preview=1 \
+    persist.vendor.camera.swtnr.preview=1 \
+    persist.vendor.camera.tnr.video=1 \
+    persist.vendor.camera.aec.sync=1 \
+    persist.vendor.camera.instant.aec=1 \
+    persist.vendor.camera.ae.instant.bound=20 \
+    persist.vendor.camera.depurple=1 \
+    persist.vendor.denoise.process.plates=2 \
+    persist.vendor.dualcam.lpm.enable=0 \
+    persist.vendor.tnr.process.plates=2 \
+    vendor.camera.cpp.set.clock=576 \
+    vendor.camera.not.cts.apk=1 \
+    vendor.camera.not.ctsverify.apk=1 \
+    vendor.camera.vidhance.eis.enabled=1 \
+    vendor.camera.vidhanceEis.force=1 \
+    vendor.vidhance.video.enabled=1
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera
+
+# Charger
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # Display
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
@@ -61,17 +121,40 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     disable_configstore
 
-PRODUCT_PRODUCT_PROPERTIES += \
-     persist.sys.sf.color_saturation=1.1
+PRODUCT_VENDOR_PROPERTIES += \
+     persist.sys.sf.color_saturation=1.1 \
+     debug.sf.disable_backpressure=1 \
+     debug.sf.enable_hwc_vds=1 \
+     debug.sf.latch_unsignaled=1 \
+     ro.surface_flinger.use_color_management=1 \
+     ro.vendor.display.sensortype=2 \
+     ro.vendor.display.svi=1 \
+     vendor.display.svi.config=1 \
+     vendor.display.svi.config_path=/vendor/etc/SVIConfig.xml
+
+PRODUCT_SYSTEM_PROPERTIES += \
+     persist.lcd.cabc_mode=1 \
+     persist.lcd.hbm_mode=0
 
 # Doze
 PRODUCT_PACKAGES += \
     ParanoidDoze
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sensor.proximity=true
+    ro.sensor.pickup=xiaomi.sensor.pickup
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpmhalservice.enable=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
+
+PRODUCT_VENDOR_PROPERTIES += \
+    drm.service.enabled=true
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -82,6 +165,10 @@ PRODUCT_COPY_FILES += \
 
 # FM
 BOARD_HAVE_QCOM_FM := true
+
+# FRP
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
 # GPS
 LOC_HIDL_VERSION := 4.0
@@ -109,6 +196,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Incremental FS
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.incremental.enable=1
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
@@ -126,6 +217,15 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.crypto.allow_encrypt_override=true \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.hardware.keystore_desede=true
+
+# Netflix
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.netflix.bsp_rev=Q6125-17995-1
+
 # Netmgr
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
@@ -136,6 +236,9 @@ PRODUCT_PACKAGES += \
 
 # NFC
 TARGET_NFC_SKU := willow
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.se.type=HCE,UICC
 
 # Overlays
 PRODUCT_PACKAGES += \
@@ -154,6 +257,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.xiaomiparts.rc
+
+# PASR
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.power.pasr.enabled=true \
+    vendor.pasr.activemode.enabled=true
 
 # Perf
 PRODUCT_COPY_FILES += \
@@ -190,6 +298,27 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.manual_nw_rej_ct=1 \
+    persist.vendor.radio.mt_sms_ack=30 \
+    persist.vendor.radio.process_sups_ind=1 \
+    persist.vendor.radio.redir_party_num=1 \
+    persist.vendor.radio.report_codec=1 \
+    persist.vendor.ssr.restart_level=ALL_ENABLE \
+    ro.telephony.default_network=22,22
+
+# SoC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI \
+    ro.soc.model=SM6125
+
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
@@ -204,9 +333,19 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.sensors.enable.bypass_worker=true \
+    persist.vendor.sensors.enable.mag_filter=true \
+    persist.vendor.sensors.enable.rt_task=false \
+    persist.vendor.sensors.support_direct_channel=false
+
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Time
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.delta_time.enable=true
 
 # Thermal
 PRODUCT_COPY_FILES += \
@@ -219,6 +358,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.data.iwlan.enable=true
+    ro.telephony.iwlan_operation_mode=legacy
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libavservices_minijail \
@@ -229,3 +372,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.debug.wfd.enable=1
+    persist.sys.wfd.virtual=0
+
+# ZRAM
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=1440 \
+    ro.zram.periodic_wb_delay_hours=24

aryaman895/device_xiaomi_ginkgo

device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
-# Copyright (C) 2020-2021 Paranoid Android
+# Copyright (C) 2020-2022 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -25,6 +25,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     vendor.audio.feature.compr_voip.enable=true \
     vendor.audio.feature.spkr_prot.enable=false
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.config.vc_call_vol_default=5 \
+    ro.config.vc_call_vol_steps=6
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
@@ -37,6 +41,21 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.scram.enabled=true \
+    persist.vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false \
+    persist.vendor.bluetooth.modem_nv_support=true \
+    ro.vendor.bluetooth.wipower=false \
+    vendor.qcom.bluetooth.soc=cherokee
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.bluetooth.bqr.event_mask=14 \
+    persist.bluetooth.bqr.min_interval_ms=500
 
 # Camera
 $(call inherit-product, vendor/miuicamera/config.mk)
@@ -54,6 +73,47 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.camera.preview.ubwc=0 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.isp.turbo=1 \
+    persist.vendor.camera.imglib.usefdlite=1 \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.mpo.disabled=1 \
+    persist.vendor.camera.manufacturer=Xiaomi \
+    persist.vendor.camera.stats.test=0 \
+    persist.vendor.camera.awb.sync=2 \
+    persist.vendor.camera.af.sync=2 \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.is_type=4 \
+    persist.vendor.camera.is_type_preview=4 \
+    persist.vendor.camera.gyro.disable=0 \
+    persist.vendor.camera.llnoise=1 \
+    persist.vendor.camera.tnr.preview=1 \
+    persist.vendor.camera.swtnr.preview=1 \
+    persist.vendor.camera.tnr.video=1 \
+    persist.vendor.camera.aec.sync=1 \
+    persist.vendor.camera.instant.aec=1 \
+    persist.vendor.camera.ae.instant.bound=20 \
+    persist.vendor.camera.depurple=1 \
+    persist.vendor.denoise.process.plates=2 \
+    persist.vendor.dualcam.lpm.enable=0 \
+    persist.vendor.tnr.process.plates=2 \
+    vendor.camera.cpp.set.clock=576 \
+    vendor.camera.not.cts.apk=1 \
+    vendor.camera.not.ctsverify.apk=1 \
+    vendor.camera.vidhance.eis.enabled=1 \
+    vendor.camera.vidhanceEis.force=1 \
+    vendor.vidhance.video.enabled=1
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera
+
+# Charger
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # Display
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
@@ -62,16 +122,41 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_PRODUCT_PROPERTIES += \
-     persist.sys.sf.color_saturation=1.1
+    persist.sys.sf.color_saturation=1.1
+
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.latch_unsignaled=1 \
+    ro.surface_flinger.use_color_management=1 \
+    ro.vendor.display.sensortype=2 \
+    ro.vendor.display.svi=1 \
+    vendor.display.svi.config=1 \
+    vendor.display.svi.config_path=/vendor/etc/SVIConfig.xml
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.lcd.cabc_mode=1 \
+    persist.lcd.hbm_mode=0
 
 # Doze
 PRODUCT_PACKAGES += \
     ParanoidDoze
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sensor.proximity=true \
+    ro.sensor.pickup=xiaomi.sensor.pickup
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpmhalservice.enable=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.3.vendor
+
+PRODUCT_VENDOR_PROPERTIES += \
+    drm.service.enabled=true
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -82,6 +167,14 @@ PRODUCT_COPY_FILES += \
 
 # FM
 BOARD_HAVE_QCOM_FM := true
+
+# FRP
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# FUSE passthrough
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
 
 # GPS
 LOC_HIDL_VERSION := 4.0
@@ -109,6 +202,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Incremental FS
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.incremental.enable=1
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
@@ -126,6 +223,15 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.crypto.allow_encrypt_override=true \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.hardware.keystore_desede=true
+
+# Netflix
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.netflix.bsp_rev=Q6125-17995-1
+
 # Netmgr
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
@@ -136,6 +242,9 @@ PRODUCT_PACKAGES += \
 
 # NFC
 TARGET_NFC_SKU := willow
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.se.type=HCE,UICC
 
 # Overlays
 PRODUCT_PACKAGES += \
@@ -154,6 +263,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.xiaomiparts.rc
+
+# PASR
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.power.pasr.enabled=true \
+    vendor.pasr.activemode.enabled=true
 
 # Perf
 PRODUCT_COPY_FILES += \
@@ -175,6 +289,27 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.manual_nw_rej_ct=1 \
+    persist.vendor.radio.mt_sms_ack=30 \
+    persist.vendor.radio.process_sups_ind=1 \
+    persist.vendor.radio.redir_party_num=1 \
+    persist.vendor.radio.report_codec=1 \
+    persist.vendor.ssr.restart_level=ALL_ENABLE \
+    ro.telephony.default_network=22,22
+
+# SoC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI \
+    ro.soc.model=SM6125
+
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
@@ -189,9 +324,19 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.sensors.enable.bypass_worker=true \
+    persist.vendor.sensors.enable.mag_filter=true \
+    persist.vendor.sensors.enable.rt_task=false \
+    persist.vendor.sensors.support_direct_channel=false
+
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Time
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.delta_time.enable=true
 
 # Thermal
 PRODUCT_COPY_FILES += \
@@ -203,3 +348,17 @@ PRODUCT_COPY_FILES += \
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.data.iwlan.enable=true \
+    ro.telephony.iwlan_operation_mode=legacy
+
+# ZRAM
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=1440 \
+    ro.zram.periodic_wb_delay_hours=24
+
+# Zygote
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true